53236957d64533c34cfd4e5f613af465958a0891
bundai223 <bundai223@gmail.com>
2015-10-13 19:08:31 +0900

    trace tutorial. But it's not working, because document is too old when tcod v0.1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate tcod;
+use tcod::{Console, background_flag, key_code, Special};
+
 fn main() {
-    println!("Hello, world!");
+    let mut con = Console::init_root(80, 50, "libtcod Rust tutorial", false);
+    let mut exit = false;
+
+    while!(Console::window_closed() || exit) {
+        con.clear();
+        con.put_char(40, 25, '@', background_flag::Set);
+        Console::flush();
+
+        let keypress = Console::wait_for_keypress(true);
+        match keypress.key {
+            Special(key_code::Escape) => exit = true,
+            _ => {}
+        }
+    }
 }

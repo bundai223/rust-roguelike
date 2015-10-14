884cef0125f68a4816434413a57e4dd177c617bb
bundai223 <bundai223@gmail.com>
2015-10-14 18:49:26 +0900

    @ can move.

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,69 @@ use tcod::console::{Console, Root, BackgroundFlag};
 use tcod::input::{KeyCode, Event, EventFlags, check_for_event};
 
 fn main() {
+    // initialize
+    let console_w = 80;
+    let console_h = 50;
+    let mut x = 40;
+    let mut y = 25;
+
     let mut root = Root::initializer()
-        .size(80, 50)
+        .size(console_w, console_h)
         .title("Roguelike in Rustlang.")
+//         .renderer(Renderer::GLSL) // maybe need shader
         .init();
-//         .renderer(Renderer::GLSL)
     let mut exit = false;
 
+    // Render
     while!(root.window_closed() || exit) {
-        root.clear();
-        root.put_char(40, 25, '@', BackgroundFlag::Set);
-        root.flush();
-
+        // polling Event
         let event = check_for_event(EventFlags::all());
         match event {
             None => {},
-            Some(x) => {
-                match x.1 {
+            Some(event_tuple) => {
+                match event_tuple.1 {
                     Event::Key(key) => {
-                        println!("key {:?}", key.code);
-                        exit = true;
+                        match key.code {
+                            KeyCode::Escape => exit = true,
+                            KeyCode::Left => {
+                                x -= 1;
+                                if x < 0 {
+                                    x = 0;
+                                }
+                            },
+                            KeyCode::Right => {
+                                x += 1;
+                                if console_w - 1 < x {
+                                    x = console_w - 1;
+                                }
+                            },
+                            KeyCode::Up => {
+                                y -= 1;
+                                if y < 0 {
+                                    y = 0;
+                                }
+                            },
+                            KeyCode::Down => {
+                                y += 1;
+                                if console_h - 1< y {
+                                    y = console_h - 1;
+                                }
+                            },
+                            _ => {}
+                        }
                     },
                     _ => {}
                 }
             }
-//             KeyCode::Escape => exit = true,
-//             _ => {}
         }
+
+        render(&mut root, x, y);
     }
+}
+
+
+fn render(root: &mut Root, x: i32, y: i32) {
+    root.clear();
+    root.put_char(x, y, '@', BackgroundFlag::Set);
+    root.flush();
 }
